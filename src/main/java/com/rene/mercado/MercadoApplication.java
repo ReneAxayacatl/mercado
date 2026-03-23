b82d4eda045ce542b5cff3e2de75a87e7b409c58
ReneAxayacatl <reaxarame@yahoo.com.mx>
2026-03-23 12:44:08 -0600

    creacion de .war y modificacion de archivo main

--- src/main/java/com/rene/mercado/MercadoApplication.java
+++ src/main/java/com/rene/mercado/MercadoApplication.java
@@ -1,13 +1,30 @@
 package com.rene.mercado;
 
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+// public class MercadoApplication {
+
+// 	public static void main(String[] args) {
+// 		SpringApplication.run(MercadoApplication.class, args);
+// 	}
+
+// }
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MercadoApplication {
+public class MercadoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MercadoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MercadoApplication.class, args);
+    }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MercadoApplication.class);
+    }
 }
